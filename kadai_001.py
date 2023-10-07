7a989d9e7e85a6fcba517b97a7d919d600a7c4f0
nakasan1977 <144198476+nakasan1977@users.noreply.github.com>
2023-10-07 16:55:36 +0900

    Update kadai_001.py

--- kadai_001.py
+++ kadai_001.py
@@ -1,12 +1,18 @@
 import openpyxl
 import datetime
+import openpyxl as px
 
 wb = openpyxl.Workbook()
 ws = wb.active
-ws["b4"] = "請求書"
-ws["b5"] = "株式会社ABC"  # type: ignore
+ws["b2"] = "請求書"
+ws["b5"] = "〒101-0022 東京都千代田区神田練塀町300"
+ws["b4"] = "株式会社ABC"  # type: ignore
 ws["b6"] = "TEL:03-1234-5678 FAX:03-1234-5678"  # type: ignore
 ws["b7"] = "担当者名:鈴木一郎 様"  # type: ignore
+
+for cell in ['B4', 'B5', 'B6', 'B7', 'F4', 'F5','g4','g5']:
+    ws[cell].font = px.styles.Font(size=10)
+
 
 ws["f4"] = "No"  # type: ignore
 ws["f5"] = "日付"  # type: ignore
@@ -14,6 +20,8 @@ ws["f5"] = "日付"  # type: ignore
 today = datetime.datetime.now()
 print(today.strftime("%Y/%m/%d"))
 ws["g5"] = today.strftime("%Y/%m/%d")
+ws["g4"] = "0001"
+
 ws["b10"] = "商品名"
 ws["c10"] = "数量"
 ws["d10"] = "単価"
@@ -36,7 +44,6 @@ for i in range(11, 16):
   i= i+1
 
 
-# E列の合計を計算
 sum_formula = f"=SUM(E11:E{current_row - 1})"
 ws[f"E{current_row}"] = sum_formula
 
@@ -48,6 +55,5 @@ ws["e15"] = sum_formula
 ws["e16"] = "=E15*0.1"
 ws["e17"] = "=SUM(E15:E16)"
 
-# ファイルを保存
-wb.save("請求書_20231003.xlsx")
 
+wb.save("請求書_20231003.xlsx")
